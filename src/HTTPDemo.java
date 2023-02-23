298b8658e31801be10aab0ec8bbb4ea171d3d29d
Dinuka Perera <dinuka.20210433@iit.ac.lk>
2023-02-24 00:30:36 +0530

    java demo on getting the current day from the HTTP port

--- src/HTTPDemo.java
+++ src/HTTPDemo.java
@@ -1,3 +1,26 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * Java program to create a simple HTTP Server to demonstrate how to use
+ * ServerSocket and Socket class.
+ *
+ * @author Javin Paul
+ */
 public class HTTPDemo {
-    
+    public static void main(String[] args) throws IOException{
+
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening to connection on port 8080.....");
+        while(true){
+            try(Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpsResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream().write(httpsResponse.getBytes("UTF-8"));
+            }
+        }
+    }
+
 }
